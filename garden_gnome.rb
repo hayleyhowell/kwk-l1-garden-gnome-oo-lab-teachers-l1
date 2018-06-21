8a7c2201bc55964dc45956a6d114caf3d863d018
Hayley Howell <hayley_howell@sbcglobal.net>
2018-06-21 15:24:32 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,8 +1,7 @@
 # Code your instances here
 class GardenGnome
   attr_accessor :name, :age, :gluten_allergy
-  attr_reader :name, :age, :gluten_allergy
-  
+
   def initialize(hat_color)
     @personality = "evil"
   end 
